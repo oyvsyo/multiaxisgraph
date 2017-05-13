03958960e197cb55d259888492d64f14f194ba1e
oyvsyo <lostedthare@gmail.com>
2017-05-13 12:25:54 +0200

    in_progress

--- mag.py
+++ mag.py
@@ -9,6 +9,19 @@ def set_graph_attributes(color=1, line_width=1):
     graph.SetLineColor(color)
     graph.SetLineWidth(line_width)
 
+def scale(bottom, top, y, default_scale=1):
+    delta_x = top-bottom
+    delta_y = max(y)-min(y)
+    if delta_y == 0:
+        alpha = 1/default_scale
+        beta = bottom
+        y_norm = [beta  for i in range(len(y))]
+    else:
+        alpha = delta_x/delta_y
+        beta = top-alpha*max(y)
+        y_norm = [y[i]*alpha+beta  for i in range(len(y))]
+    return [alpha, beta, arr(y_norm)]
+
 # def set_axis_attributes(axis, x1, y1, x2, y2, , ):
 
 class MultiAxisGraphError(Exception):
@@ -39,27 +52,42 @@ class MultiAxisGraph(ROOT.TObject):
         self.pad_width = 0.08
         self.pad = ROOT.TPad("pad0_"+self.name, "pad0_"+self.name, .0, .0, 1, 1., 45, -1, -2)
         self.graph = ROOT.TMultiGraph()
+        self.right_axis = 0
         self.axis = []
         self.pads = []
         self.graphs = []
+        self.bottom = +99999999.
+        self.top = -99999999.
 
-    def add_graph(self, x=[], y=[], color=1, line_width=1, use_additional_axis=0):
-        self.graphs += ROOT.TGraph(len(x), x, y)
-        i = len(self.graphs)
-        self.graphs[i].SetName("graph_%i" %i)
-        set_graph_attributes(self.graphs[i], color, line_width)
-        if use_additional_axis==1:
+    def AddGraph(self, x=[], y=[], color=1, line_width=1, use_axis=0):
+        self.AddBaseGraph(x, y, color, line_width)
+        if use_axis==1:
             k = len(self.pads)
             self.pads += ROOT.TPad()
             self.axis += ROOT.TGaxis()
+        if use_axis==-1:
+            max_x = max(x)
+            [alpha, beta, y_scale] = scale(self.bottom, self.top, y)
+            self.right_axis = ROOT.TGaxis(max_x, 0.1, max_x, 0.9, )
 
-    def set_pads(self):
+
+    def AddBaseGraph( x=[], y=[], color=1, line_width=1,):
+        self.graphs += ROOT.TGraph(len(x), x, y)
+        i = len(self.graphs)-1
+        self.graphs[i].SetName("graph_%i" %i)
+        if self.bottom > min(y):
+            self.bottom = min(y)
+        if self.top < max(y):
+            self.top = max(y)
+        set_graph_attributes(self.graphs[i], color, line_width)
+
+    def SetPads(self):
         k = len(self.pads)
         self.pad_width = 1-0.5/(k+1)
         self.pad.SetPad(pad_width, 0., 0., 1.)
-        map(lambda p: p.SetPad((1-self.pad_width))
+        map(lambda p: p.SetPad((1-self.pad_width)))
 
-    def SetTitle(self, title):
+    def SetTitle(self, title=0):
         try:
             self.graph.SetTitle(title)
         except Exception:
@@ -79,6 +107,7 @@ class MultiAxisGraph(ROOT.TObject):
 
     def Draw(self):
         map(lambda pad_item: pad_item.Draw, self.pads)
+        self.pad.Draw()
         self.pad.cd()
         self.pad.SetRightMargin(0.01)
         self.pad.SetLeftMargin(0.12)
@@ -96,13 +125,13 @@ c = ROOT.TCanvas()
 
 s = MultiAxisGraph()
 # sig.set_graph1()
-# s.add_graph(x_t, y_t)
+# s.AddGraph(x_t, y_t)
 #
-# s.add_graph(x_i, y_i)
+# s.AddGraph(x_i, y_i)
 #
-# s.add_graph(x_u, y_u)
+# s.AddGraph(x_u, y_u)
 #
-# s.Draw()
+s.Draw()
 # c2 = ROOT.TCanvas()
 # sig2 = MultiAxisGraph("kek")
 # sig2.Draw()
