809c2a68bd55deca72dba5ebc71bc9ed48c02588
oyvsyo <lostedthare@gmail.com>
2017-07-26 18:12:42 +0300

    pep8 stylish mag.py

--- mag.py
+++ mag.py
@@ -2,8 +2,10 @@ import ROOT
 from array import array
 from copy import deepcopy
 
+
 def arr(x):
     return array("d", x)
+
 
 def set_graph_attributes(graph, color=1, line_width=1, name="", title=""):
     try:
@@ -16,24 +18,28 @@ def set_graph_attributes(graph, color=1, line_width=1, name="", title=""):
     except Exception:
         raise UnificatedMAGError
 
+
 def set_graph_y(graph, y):
     gety = graph.GetY()
     for i in range(graph.GetN()):
         gety[i] = y[i]
+
 
 def get_graph_y(graph):
     """returns a list of y's coordinates of points in graph"""
     gety = graph.GetY()
     return map(lambda i: gety[i], range(graph.GetN()))
 
+
 def add_graphs(multigraph, graphs):
     """adds graphs to multigraph"""
     map(lambda g: multigraph.Add(g), graphs)
 
+
 def scale(bottom, top, min_y, max_y, default_scale=1):
     """scale linear transformation from bottom top to y_min, y_max"""
-    delta_x = top-bottom
-    delta_y = max_y -min_y
+    delta_x = top - bottom
+    delta_y = max_y - min_y
     if delta_y == 0:
         alpha = 1/default_scale
         beta = bottom
@@ -42,11 +48,13 @@ def scale(bottom, top, min_y, max_y, default_scale=1):
         beta = top-alpha*max_y
     return [alpha, beta]
 
+
 def range_y(ys):
     """returns min(min()) & max(max()) of list of lists"""
     top = max(map(lambda y: max(y), ys))
     bottom = min(map(lambda y: min(y), ys))
     return [bottom, top]
+
 
 def linear_transformation(a, b, y):
     return map(lambda i: a*i+b, y)
@@ -60,11 +68,11 @@ class MultiAxisGraphError(Exception):
         super(MultiAxisGraphError, self).__init__(msg)
         self.MAG_object = MAG_object
 
+
 class UnificatedMAGError(MultiAxisGraphError):
     """docstring for UnificatedMAGError."""
     def __init__(self, msg):
         super(UnificatedMAGError, self).__init__(msg)
-
 
 
 class MultiAxisGraph(ROOT.TObject):
@@ -77,8 +85,8 @@ class MultiAxisGraph(ROOT.TObject):
             self.name = name
         else:
             self.name = str(self.GetUniqueID())
-        self.graphs = {"base":[], "right":[], "left":[]}
-        self.axis = {"left":[], "right":[]}
+        self.graphs = {"base": [], "right": [], "left": []}
+        self.axis = {"left": [], "right": []}
         self.pads = []
         self.graph = ROOT.TMultiGraph()
         self.scale_coefs = {}
@@ -107,13 +115,18 @@ class MultiAxisGraph(ROOT.TObject):
 
     def __scale(self):
         """create new info about scaled graphs"""
-        [bottom, top] = range_y(map(lambda graph: get_graph_y(graph), self.graphs["right"]))
-        [y_base_min, y_base_max] = range_y(map(lambda graph: get_graph_y(graph), self.graphs["base"]))
+        [bottom, top] = range_y(map(lambda graph: get_graph_y(graph),
+                                self.graphs["right"]))
+        [y_base_min, y_base_max] = range_y(map(lambda graph:
+                                           get_graph_y(graph),
+                                           self.graphs["base"]))
         [a, b] = scale(y_base_min, y_base_max, bottom, top)
         self.scale_coefs["right"] = [a, b]
         self.graphs["right_scaled"] = deepcopy(self.graphs["right"])
         n = len(self.graphs["right"])
-        map(lambda i: set_graph_y(self.graphs["right_scaled"][i], linear_transformation(a, b,get_graph_y(self.graphs["right"][i]))), range(n))
+        map(lambda i: set_graph_y(self.graphs["right_scaled"][i],
+            linear_transformation(a, b, get_graph_y(self.graphs["right"][i]))),
+            range(n))
         y_axis_min = self.graph.GetYaxis().GetXmin()
         y_axis_max = self.graph.GetYaxis().GetXmax()
         x_axis_max = self.graph.GetXaxis().GetXmax()
@@ -132,10 +145,10 @@ class MultiAxisGraph(ROOT.TObject):
         self.axis["right"].SetLabelSize(0.035)
 
     def __str__(self):
-        return "MultiAxisGraph object - %s " %self.name
+        return "MultiAxisGraph object - %s " % self.name
 
     def __repr__(self):
-        return "<MultiAxisGraph object at 0x%0x>" %id(self)
+        return "<MultiAxisGraph object at 0x%0x>" % id(self)
 
     def __enter__(self):
         return self
@@ -148,7 +161,7 @@ class MultiAxisGraph(ROOT.TObject):
         """Create all needed objects and draw them all """
         add_graphs(self.graph, self.graphs["base"])
         self.graph.Draw("AL")
-        if len(self.graphs["right"])!=0:
+        if len(self.graphs["right"]) != 0:
             self.__scale()
             add_graphs(self.graph, self.graphs["right_scaled"])
             self.graph.Draw("AL")
@@ -157,6 +170,6 @@ class MultiAxisGraph(ROOT.TObject):
 
     def Update(self):
         self.graph.Draw("AL")
-        if len(self.graphs["right"])!=0:
+        if len(self.graphs["right"]) != 0:
             self.axis["right"].Draw()
         ROOT.gPad.Update()
